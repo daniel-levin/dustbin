be7a693b1ab8b9a0656bc92f984fae0983865d4c
Daniel Levin <daniellevin2607@gmail.com>
2026-08-31 18:23:26 +0200

    add example

---
+++ elf_tests/tests/text.rs
@@ -0,0 +1,19 @@
+use dustbin_elf::analysis::text::x86::*;
+use dustbin_elf::analysis::*;
+
+#[test]
+fn check_bash_main() -> anyhow::Result<()> {
+    let a = Analyze64Le::begin(elf_tests::linux_bash_x86_64)?;
+
+    let main = a.function_definitions().lookup(b"main").unwrap();
+
+    let df = DecodedFuncDefn::decode(&main).unwrap();
+
+    for i in df.ins.iter() {
+        assert!(i.mnemonic() != iced_x86::Mnemonic::Pclmulqdq);
+    }
+
+    assert_eq!(main.address, 0x2ae0);
+
+    Ok(())
+}
