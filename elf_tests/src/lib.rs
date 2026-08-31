3c946d49216e1a1aeda9cdc6a89ff51a36cc1b95
Daniel Levin <daniellevin2607@gmail.com>
2026-08-31 18:20:01 +0200

    provide checked in binary to tests

--- elf_tests/src/lib.rs
+++ elf_tests/src/lib.rs
@@ -1,1 +1,7 @@
+#![allow(non_upper_case_globals)]
 
+//! ELF binaries used by tests.
+//!
+//! Each regular file in the workspace's `bins` directory is exported as a byte slice.
+
+include!(concat!(env!("OUT_DIR"), "/bins.rs"));
